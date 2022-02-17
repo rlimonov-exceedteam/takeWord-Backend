a5520b362e1b12a5366280550d9f657251dd86bd
Roman <93902664+rlimonov-exceedteam@users.noreply.github.com>
2022-02-17 13:54:16 +0300

    Update index.js

--- src/db/models/refreshToken-schema/index.js
+++ src/db/models/refreshToken-schema/index.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require('mongoose');
 
 const TokenSchema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: 'user'},
+    user: {type: Schema.Types.ObjectId, ref: 'userSchema'},
     refreshToken: {type: String, required: true},
 });
 
